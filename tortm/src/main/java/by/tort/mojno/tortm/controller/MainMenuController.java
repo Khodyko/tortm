85a7a9c571cc82419ae13795896e2266b6c84902
Igor <gaes1k@yandex.ru>
2023-12-31 00:41:20 +0300

    init: add docker initialisation

--- tortm/src/main/java/by/tort/mojno/tortm/controller/MainMenuController.java
+++ tortm/src/main/java/by/tort/mojno/tortm/controller/MainMenuController.java
@@ -1,20 +1,20 @@
 package by.tort.mojno.tortm.controller;
 
 import by.tort.mojno.tortm.model.cake.Cream;
+import by.tort.mojno.tortm.model.common.CostMeasurementEnum;
 import by.tort.mojno.tortm.service.CreamService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MainMenuController {
+
+//    Logger logger = (Logger) LoggerFactory.getLogger(MainMenuController.class);
 
     private final CreamService creamService;
 
@@ -22,13 +22,28 @@ public class MainMenuController {
         this.creamService = creamService;
     }
 
+    //Controller only for check
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
-    public ModelAndView getMainMenuInfo(){
-        ModelAndView modelAndView=new ModelAndView("index");
-        List<Cream> creamList= creamService.getAllCreams();
-        System.out.println(creamList.getFirst().getDescription());
-        modelAndView.addObject("creams",creamList);
+    public ModelAndView getMainMenuInfo() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        List<Cream> creamList = creamService.getAllCreams();
+        if (creamList != null && creamList.size()>0) {
+            System.out.println(creamList.getFirst().getDescription());
+        }
+        modelAndView.addObject("creams", creamList);
+        return modelAndView;
+    }
+
+    //Controller only for check
+    @GetMapping(value = "/cream")
+    @ResponseStatus(value = HttpStatus.OK)
+    public ModelAndView addCream() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        Cream cream = new Cream();
+        cream.setDescription("классный торт");
+        cream.setCostMeasurement(CostMeasurementEnum.WEIGHT_OF_PARENT);
+        creamService.addCream(cream);
         return modelAndView;
     }
 }
